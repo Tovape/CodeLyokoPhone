c9863101a7d02e2e3aa9cd7415508ed2968bfd1a
Tovape <tonivalverde0@gmail.com>
2022-05-07 23:59:04 +0200

    Added background, svgs, color palette

--- CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
+++ CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
@@ -5,59 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ImageButton;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    // request Perms for app
+    // Request Permission for app
     private static final int REQUEST_CALL = 1;
 
-    EditText edittext1;
-    Button button1;
+    // Variables
+    EditText numberInput;
+    ImageButton button1;
+
+    // Main Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Getting the edittext and button instance
-        edittext1=(EditText)findViewById(R.id.editText1);
-        button1=(Button)findViewById(R.id.button1);
+        // Getting Variable Instances
+        numberInput = (EditText)findViewById(R.id.numberInput);
+        button1 = (ImageButton)findViewById(R.id.button1);
 
-        //Performing action on button click
+        // Performing action on button click
         button1.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                String number = edittext1.getText().toString();
-                if (number.trim().length() > 0) {
-                    if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                        edittext1.setText("No Permission to call");
-                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-                    } else {
-                        // Make Phone Call
+                String number = numberInput.getText().toString();
+
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                    numberInput.setText("Grant Permission First");
+                } else {
+                    // Make Phone Call
+                    if (number.trim().length() > 0) {
                         Intent callIntent = new Intent(Intent.ACTION_CALL);
                         callIntent.setData(Uri.parse("tel:"+number));
                         startActivity(callIntent);
                     }
-                } else {
-                    edittext1.setText("Phone Short?");
                 }
-
             }
-
         });
     }
 }
