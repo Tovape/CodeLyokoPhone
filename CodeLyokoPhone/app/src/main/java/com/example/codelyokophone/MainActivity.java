8a381a15c2a7c44dd875f61be263b42d92cc0825
Tovape <tonivalverde0@gmail.com>
2022-05-15 02:16:34 +0200

    Changed from Relative mode to Constraint + LinearLayout for percentages

--- CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
+++ CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.codelyokophone;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
