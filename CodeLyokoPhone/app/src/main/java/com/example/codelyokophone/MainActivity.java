5c9c71d32027a12adfd62ad8ec19be4852a83f74
Tovape <tonivalverde0@gmail.com>
2022-05-17 18:16:54 +0200

    Added animations and new apk
    Tried using setBackground and setImageResource variants but had lots of issues, used a button set state instead with some sleeping method

--- CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
+++ CodeLyokoPhone/app/src/main/java/com/example/codelyokophone/MainActivity.java
@@ -2,34 +2,181 @@ package com.example.codelyokophone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Handler;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     // Request Permission for app
     private static final int REQUEST_CALL = 1;
+    private int delayIncrement = 140;
+    // Other
+    private ScheduledExecutorService scheduleTaskExecutor;
+    Context context = this;
+
+    // Methods
+    public void PerformAnimation(int[] digitGetter, int i, int delayer, Handler handlerAnimation, int delayIncrement) {
+        // Haven't found a way to optimise this yet
+        if (digitGetter[i] == 1) {
+            buttonOne.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonOne.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonOne.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonOne.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 2) {
+            buttonTwo.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonTwo.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonTwo.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonTwo.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 3) {
+            buttonThree.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonThree.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonThree.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonThree.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 4) {
+            buttonFour.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonFour.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonFour.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonFour.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 5) {
+            buttonFive.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonFive.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonFive.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonFive.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 6) {
+            buttonSix.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonSix.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonSix.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonSix.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 7) {
+            buttonSeven.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonSeven.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonSeven.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonSeven.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 8) {
+            buttonEight.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonEight.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonEight.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonEight.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 9) {
+            buttonNine.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonNine.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonNine.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonNine.setPressed(false);
+                }
+            }, delayer);
+        } else if (digitGetter[i] == 0) {
+            buttonZero.setPressed(true);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonZero.setPressed(true);
+                }
+            }, delayer - this.delayIncrement);
+            buttonZero.setPressed(false);
+            handlerAnimation.postDelayed(new Runnable() {
+                public void run() {
+                    buttonZero.setPressed(false);
+                }
+            }, delayer);
+        }
+    }
 
     // Variables
 
-    // Number Input
+    // EditText
     EditText numberInput;
+    EditText downloadSpeed;
+    EditText uploadSpeed;
 
     // Slide Options
     ImageButton buttonFwr;
@@ -64,30 +211,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Getting Variable Instances
-        numberInput = (EditText)findViewById(R.id.numberInput);
-        buttonDone = (ImageButton)findViewById(R.id.buttonDone);
-        buttonZero = (ImageButton)findViewById(R.id.buttonZero);
-        buttonOne = (ImageButton)findViewById(R.id.buttonOne);
-        buttonTwo = (ImageButton)findViewById(R.id.buttonTwo);
-        buttonThree = (ImageButton)findViewById(R.id.buttonThree);
-        buttonFour = (ImageButton)findViewById(R.id.buttonFour);
-        buttonFive = (ImageButton)findViewById(R.id.buttonFive);
-        buttonSix = (ImageButton)findViewById(R.id.buttonSix);
-        buttonSeven = (ImageButton)findViewById(R.id.buttonSeven);
-        buttonEight = (ImageButton)findViewById(R.id.buttonEight);
-        buttonNine = (ImageButton)findViewById(R.id.buttonNine);
-        buttonClear = (ImageButton)findViewById(R.id.buttonClear);
-        buttonCrew = (ImageButton)findViewById(R.id.buttonCrew);
+        numberInput = (EditText) findViewById(R.id.numberInput);
+        buttonCall = (ImageButton) findViewById(R.id.buttonCall);
+        buttonZero = (ImageButton) findViewById(R.id.buttonZero);
+        buttonOne = (ImageButton) findViewById(R.id.buttonOne);
+        buttonTwo = (ImageButton) findViewById(R.id.buttonTwo);
+        buttonThree = (ImageButton) findViewById(R.id.buttonThree);
+        buttonFour = (ImageButton) findViewById(R.id.buttonFour);
+        buttonFive = (ImageButton) findViewById(R.id.buttonFive);
+        buttonSix = (ImageButton) findViewById(R.id.buttonSix);
+        buttonSeven = (ImageButton) findViewById(R.id.buttonSeven);
+        buttonEight = (ImageButton) findViewById(R.id.buttonEight);
+        buttonNine = (ImageButton) findViewById(R.id.buttonNine);
+        buttonClear = (ImageButton) findViewById(R.id.buttonClear);
+        buttonCrew = (ImageButton) findViewById(R.id.buttonCrew);
+        downloadSpeed = (EditText) findViewById(R.id.downloadSpeed);
+        uploadSpeed = (EditText) findViewById(R.id.uploadSpeed);
+        ImageView footer = (ImageView) findViewById(R.id.footer);
+        ProgressBar downloadRate = (ProgressBar) findViewById(R.id.downloadRate);
+        ProgressBar uploadRate = (ProgressBar) findViewById(R.id.uploadRate);
+
+        // Set Footer Animation
+
+        AnimationDrawable footerAnimation;
+        footer.setBackgroundResource(R.drawable.footeranimation);
+        footerAnimation = (AnimationDrawable) footer.getBackground();
 
         // Set Number Input as Number Field with custom Font
         numberInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
 
+        // Set Fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         // Applying font
         Typeface font = Typeface.createFromAsset(getAssets(), "font/Square.ttf");
         numberInput.setTypeface(font);
+        downloadSpeed.setTypeface(font);
+        uploadSpeed.setTypeface(font);
 
         // Enabling Read Only in numberInput
         numberInput.setEnabled(false);
+        downloadSpeed.setEnabled(false);
+        uploadSpeed.setEnabled(false);
 
         // Button Actions
 
@@ -168,39 +333,103 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonCrew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberInput.append("+");
-            }
-        });
-
         // Internet Status
-
-        ConnectivityManager connectivityManager = (ConnectivityManager)this.getSystemService(CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
         NetworkCapabilities nc = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
-        int downSpeed = nc.getLinkDownstreamBandwidthKbps();
-        int upSpeed = nc.getLinkUpstreamBandwidthKbps();
+        scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
+        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+            public void run() {
 
-        Log.d("Download Speed: ", Integer.toString(downSpeed));
-        Log.d("Upload Speed: ", Integer.toString(upSpeed));
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        int downSpeed = nc.getLinkDownstreamBandwidthKbps() / 1024;
+                        int upSpeed = nc.getLinkUpstreamBandwidthKbps() / 1024;
+                        downloadSpeed.setText(Integer.toString(downSpeed) + "MB/SEC");
+                        uploadSpeed.setText(Integer.toString(upSpeed) + "MB/SEC");
 
-        // Performing action on button click
-        buttonDone.setOnClickListener(new View.OnClickListener(){
+                        if (downSpeed >= 100) {
+                            downloadRate.setProgress(100);
+                        } else {
+                            downloadRate.setProgress(downSpeed);
+                        }
+
+                        if (upSpeed >= 100) {
+                            uploadRate.setProgress(100);
+                        } else {
+                            uploadRate.setProgress(upSpeed);
+                        }
+
+                        //Log.i("LOGCAT downSpeed: \n", Integer.toString(downSpeed));
+                        //Log.i("LOGCAT upSpeed: \n", Integer.toString(upSpeed));
+                    }
+                });
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+
+        // Call Activity
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+
+        // Call
+        buttonCall.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                String number = numberInput.getText().toString();
 
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // Footer Animation
+                footerAnimation.start();
+
+                // Animation Before Call
+
+                String stringNumber = numberInput.getText().toString();
+                int intNumber = Integer.parseInt(stringNumber);
+                int numberLength = stringNumber.length();
+                int[] digitGetter = new int[15];
+
+                Log.i("LOGCAT intNumber: ", Integer.toString(intNumber));
+                Log.i("LOGCAT numberLength: ", Integer.toString(numberLength));
+
+                int delayer = 0;
+
+                if (stringNumber.trim().length() > 0) {
+
+                    Handler handlerAnimation = new Handler();
+
+                    for (int i = 0; i < numberLength; i++) {
+                        digitGetter[i] = Integer.parseInt(Integer.toString(intNumber).substring(i, i+1));
+                        delayer = delayer + delayIncrement;
+                        PerformAnimation(digitGetter, i, delayer, handlerAnimation, delayIncrement);
+                    }
+                    delayer = delayer + delayIncrement;
+                    buttonCall.setPressed(true);
+                        handlerAnimation.postDelayed(new Runnable() {
+                            public void run() {
+                                buttonCall.setPressed(true);
+                            }
+                        }, delayer - delayIncrement);
+                    buttonCall.setPressed(false);
+                        handlerAnimation.postDelayed(new Runnable() {
+                            public void run() {
+                                buttonCall.setPressed(false);
+                            }
+                        }, delayer);
+                    }
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
                 } else {
                     // Make Phone Call
-                    if (number.trim().length() > 0) {
-                        Intent callIntent = new Intent(Intent.ACTION_CALL);
-                        callIntent.setData(Uri.parse("tel:"+number));
-                        startActivity(callIntent);
-                    }
+                    Handler handlerCall = new Handler();
+                    handlerCall.postDelayed(new Runnable() {
+                        public void run() {
+                            if (stringNumber.trim().length() > 0) {
+                                footerAnimation.stop();
+                                callIntent.setData(Uri.parse("tel:"+stringNumber));
+                                startActivity(callIntent);
+                            } else {
+
+                            }
+                        }
+                    }, delayer);
                 }
             }
         });
